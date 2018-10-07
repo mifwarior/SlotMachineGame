52cf4605a28fb12b62b4a6b25a5ebc1c12aef5ab
Yuri Prosckurin <mifwarior@gmail.com>
2018-10-07 15:30:38 +0300

    Position light

--- src/components/renderers/ThreeJSRenderer.js
+++ src/components/renderers/ThreeJSRenderer.js
@@ -21,7 +21,7 @@ export default class ThreeJSRenderer extends React.Component {
     lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 
     lights[ 0 ].position.set( 0, 200, 0 );
-    lights[ 1 ].position.set( 100, 200, 100 );
+    lights[ 1 ].position.set( 100, -100, 100 );
     lights[ 2 ].position.set( - 100, - 200, - 100 );
 
     this.scene.add( lights[ 0 ] );
